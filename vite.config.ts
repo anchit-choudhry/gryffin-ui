da1f1aae960c788a6a65102f86e1a18fbc4864fe
Anchit Choudhry <5533940+anchit-choudhry@users.noreply.github.com>
2025-04-29 01:05:49 -0400

    1. Updates for GitHub pages 2. Refactored and formatted code

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import packageJson from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: packageJson.name,
   plugins: [react(), tailwindcss(),
   ],
 });
